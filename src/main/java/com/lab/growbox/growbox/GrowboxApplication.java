e3f4f8eb5dd383fb4261f61a28f18eaf903e6578
jkomarova <koyukomarova@gmail.com>
2018-10-27 12:52:12 +0300

    initial commit

---
+++ src/main/java/com/lab/growbox/growbox/GrowboxApplication.java
@@ -0,0 +1,12 @@
+package com.lab.growbox.growbox;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GrowboxApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GrowboxApplication.class, args);
+	}
+}
