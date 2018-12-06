e56afc9bdbe05e8d6e5cf2630a11bb4678d6b878
StukalovSS <sergei_stukalov@practice.relex.ru>
2018-12-06 14:51:41 +0300

    Поливалка восстанавливает полив после долива воды, если вода закончилась во время полива (без учета времени и продолжительности полива)

--- LAB/LAB.ino
+++ LAB/LAB.ino
@@ -29,7 +29,7 @@ struct PlantInfo {
 };
 
 /*
-Сделать:
+  Сделать:
   - таймер, по которому будет управляться полив
   - предусмотреть сохранение данных о растениях
   - предусм. возможность подключения нескольких растений (и компл. датчиков)
@@ -39,7 +39,7 @@ struct PlantInfo {
 #define DHTTYPE DHT21   // DHT 21 (AM2301)
 
 #define ECHOPIN 9
-#define TRIGPIN 8 
+#define TRIGPIN 8
 #define DHTPIN 2
 #define WATERING_PIN 7
 
@@ -52,8 +52,12 @@ struct PlantInfo {
 DHT tempHudtmSensor(DHTPIN, DHTTYPE);
 DS3231  rtc(SDA, SCL);
 
-float MIN_WATER_LEVEL = 5.0;
+float MIN_WATER_LEVEL = 5.0; //в сантиметрах высота погруженной помпы - уровень, ниже которого опускаться нельзя
+float TANK_HEIGHT = 20.0;    //в сантиметрах высота от дна банки до датчика расстояния
 byte errors = 0; // код ошибки (0 - ok, 1 - no water, 2 -no sensors avaiable, 3 - no connection)
+
+boolean isWateringInProcess = false;
+boolean hasErrorsWhileWatering = false;
 
 //данные о растениях, пр, тд
 DeviceStateData currentDevState; // данные о состоянии поливалки
@@ -66,7 +70,7 @@ void setup() {
 
   rtc.begin();
   tempHudtmSensor.begin();
-  
+
   pinMode(ECHOPIN, INPUT);
   pinMode(TRIGPIN, OUTPUT);
   pinMode(WATERING_PIN, OUTPUT);
@@ -74,7 +78,7 @@ void setup() {
 
   pinMode(STAT_LED_WATER, OUTPUT);
   pinMode(STAT_LED_NOCONNECT, OUTPUT);
-  
+
   checkState();
 }
 
@@ -87,7 +91,7 @@ void loop() {
 }
 
 // временная функция ----------------------------------------------------------------
-void sendDebugInfo(){
+void sendDebugInfo() {
   float h = tempHudtmSensor.readHumidity();
   float t = tempHudtmSensor.readTemperature();
   Serial.print("Temp ");
@@ -102,7 +106,7 @@ void sendDebugInfo(){
   Serial.print(" ground hudtm: ");
   Serial.println(checkGroundHum());
 
-   // Send date
+  // Send date
   Serial.print(rtc.getDateStr());
   Serial.print(" ");
   // Send time
@@ -112,76 +116,89 @@ void sendDebugInfo(){
 }
 //----------------------------------------------------------------------------------------
 //отправка данных о растении
-void sendPlantData(PlantStateData data){
+void sendPlantData(PlantStateData data) {
   Serial.println();
 }
 
 //отправка данных о состоянии устройства
-void sendStatusData(){
+void sendStatusData() {
   Serial.println();
 }
 
 //получение данных о новом растении
-void getPlantData(){
-  
+void getPlantData() {
+
 }
 
 
 //полив
-boolean watering(boolean isWatering, byte wateringPort){
+boolean watering(boolean isWatering) {
   /*
-   * если бак не пустой, то поливаем 
-   */
-   if(isWatering && checkWaterLevel() > MIN_WATER_LEVEL){
-     digitalWrite(wateringPort, LOW);
-     return true;
-   } else {
-     digitalWrite(wateringPort, HIGH);
-     return false;
-   }
+     если бак не пустой, то поливаем
+  */
+  if (isWatering && checkWaterLevel() > MIN_WATER_LEVEL) {
+    digitalWrite(WATERING_PIN, LOW);
+    isWateringInProcess = true;
+    return true;
+  } else {
+    digitalWrite(WATERING_PIN, HIGH);
+    isWateringInProcess = false;
+    return false;
+  }
 }
 
 //-------------------------------------------------------------------------------
 // проверка остатка воды
-float checkWaterLevel(){
-  float duration, cm; 
-  digitalWrite(TRIGPIN, LOW); 
-  delayMicroseconds(2); 
-  digitalWrite(TRIGPIN, HIGH); 
-  delayMicroseconds(10); 
-  digitalWrite(TRIGPIN, LOW); 
-  duration = pulseIn(ECHOPIN, HIGH); 
-  cm = duration / 58;
+float checkWaterLevel() {
+  float duration, currentVolume;
+  digitalWrite(TRIGPIN, LOW);
+  delayMicroseconds(2);
+  digitalWrite(TRIGPIN, HIGH);
+  delayMicroseconds(10);
+  digitalWrite(TRIGPIN, LOW);
+  duration = pulseIn(ECHOPIN, HIGH);
+  //инвертим для получения остаточного уровня воды
+  currentVolume = TANK_HEIGHT - (duration / 58);
 
-  if (cm <= MIN_WATER_LEVEL){
-    watering(false, WATERING_PIN);
+  if (currentVolume <= MIN_WATER_LEVEL) {
+    watering(false);
   }
-  
-  return cm;
+
+  return currentVolume;
 }
 
-// проверка яркости 
-int checkBrightness(){
+// проверка яркости
+int checkBrightness() {
   int brightness = 1024 - analogRead(BRIGHT_PIN);
   return brightness;
 }
 
-// проверка влажности почвы 
-int checkGroundHum(){
+// проверка влажности почвы
+int checkGroundHum() {
   return analogRead(HUM_PIN);
 }
 
-void checkState(){
-  if (Serial){
-     digitalWrite(STAT_LED_NOCONNECT, LOW);
+void checkState() {
+  if (Serial) {
+    digitalWrite(STAT_LED_NOCONNECT, LOW);
   } else {
     digitalWrite(STAT_LED_NOCONNECT, HIGH);
     currentDevState.errorNum = 3;
   }
   currentDevState.waterLevel = checkWaterLevel();
-  if (currentDevState.waterLevel <= MIN_WATER_LEVEL){
+  if (currentDevState.waterLevel <= MIN_WATER_LEVEL) {
+    hasErrorsWhileWatering = isWateringInProcess;
+    isWateringInProcess = false;
     digitalWrite(STAT_LED_WATER, HIGH);
   } else {
     digitalWrite(STAT_LED_WATER, LOW);
+  }
+}
+
+void checkErrorsAndRepair() {
+  //восстанавливаем работу после долива воды
+  if (currentDevState.waterLevel > MIN_WATER_LEVEL && hasErrorsWhileWatering){
+    watering(true);
+    hasErrorsWhileWatering = false;
   }
 }
